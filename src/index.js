edec1ccb755f874f5d3bdb23d66c934059620ff4
Dubemsky <122183811+Dubemsky@users.noreply.github.com>
2023-11-09 16:38:42 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,40 @@
-const express = require('express');
-const app = express();
-const db = require('./persistence');
-const getItems = require('./routes/getItems');
-const addItem = require('./routes/addItem');
-const updateItem = require('./routes/updateItem');
-const deleteItem = require('./routes/deleteItem');
+function App() {
+    const { Container, Row, Col } = ReactBootstrap;
+    const [completedCount, setCompletedCount] = React.useState(0);
+    const [incompletedCount, setIncompletedCount] = React.useState(0);
 
-app.use(express.json());
-app.use(express.static(__dirname + '/static'));
+    return (
+        <Container>
+            <Row>
+                <Col md={{ offset: 3, span: 6 }}>
+                    <TodoListCard
+                        setCompletedCount={setCompletedCount}
+                        setIncompletedCount={setIncompletedCount}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12} className="text-center">
+                    <div>Completed: {completedCount}</div>
+                    <div>Incompleted: {incompletedCount}</div>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
 
-app.get('/items', getItems);
-app.post('/items', addItem);
-app.put('/items/:id', updateItem);
-app.delete('/items/:id', deleteItem);
-
-db.init().then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000'));
-}).catch((err) => {
-    console.error(err);
-    process.exit(1);
-});
-
-const gracefulShutdown = () => {
-    db.teardown()
-        .catch(() => {})
-        .then(() => process.exit());
-};
-
-process.on('SIGINT', gracefulShutdown);
-process.on('SIGTERM', gracefulShutdown);
-process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
+function TodoListCard({ setCompletedCount, setIncompletedCount }) {
+    const [items, setItems] = React.useState([]);
+   
+    React.useEffect(() => {
+        fetch('/items')
+            .then((r) => r.json())
+            .then((data) => {
+                setItems(data);
+                // Calculate counts for completed and incompleted tasks
+                const completedTasks = data.filter((item) => item.completed);
+                const incompletedTasks = data.filter((item) => !item.completed);
+                setCompletedCount(completedTasks.length);
+                setIncompletedCount(incompletedTasks.length);
+            });
+    }, [setCompletedCount, setIncompletedCount]);
