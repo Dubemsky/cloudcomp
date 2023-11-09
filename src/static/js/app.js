042b5aca97c0940459e9bac47b7d0f2f15fddde0
Dubemsky <122183811+Dubemsky@users.noreply.github.com>
2023-11-09 16:40:58 +0000

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,20 +1,41 @@
-let displayValue = '';
 
-function appendToDisplay(value) {
-    displayValue += value;
-    document.getElementById('display').value = displayValue;
+function App() {
+    const { Container, Row, Col } = ReactBootstrap;
+    const [completedCount, setCompletedCount] = React.useState(0);
+    const [incompletedCount, setIncompletedCount] = React.useState(0);
+
+    return (
+        <Container>
+            <Row>
+                <Col md={{ offset: 3, span: 6 }}>
+                    <TodoListCard
+                        setCompletedCount={setCompletedCount}
+                        setIncompletedCount={setIncompletedCount}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12} className="text-center">
+                    <div>Completed: {completedCount}</div>
+                    <div>Incompleted: {incompletedCount}</div>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
-function clearDisplay() {
-    displayValue = '';
-    document.getElementById('display').value = displayValue;
-}
-
-function calculateResult() {
-    try {
-        displayValue = eval(displayValue);
-        document.getElementById('display').value = displayValue;
-    } catch (error) {
-        document.getElementById('display').value = 'Error';
-    }
-}
+function TodoListCard({ setCompletedCount, setIncompletedCount }) {
+    const [items, setItems] = React.useState([]);
+   
+    React.useEffect(() => {
+        fetch('/items')
+            .then((r) => r.json())
+            .then((data) => {
+                setItems(data);
+                // Calculate counts for completed and incompleted tasks
+                const completedTasks = data.filter((item) => item.completed);
+                const incompletedTasks = data.filter((item) => !item.completed);
+                setCompletedCount(completedTasks.length);
+                setIncompletedCount(incompletedTasks.length);
+            });
+    }, [setCompletedCount, setIncompletedCount]);
